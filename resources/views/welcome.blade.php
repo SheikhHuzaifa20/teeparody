52c95f5bbddd5e980bec6ad6b425ddd7a6dd6bff
Sheikh Huzaifa Farooqi <155949017+SheikhHuzaifa20@users.noreply.github.com>
2026-09-26 01:18:55 +0500

    new changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
                         }
                     @endphp
                     <div class="col-lg-4 col-md-4 col-6 mb-4">
-                        <div class="product-main {{ $loop->iteration % 2 != 0 ? 'bg-blk' : '' }}">
+                        <div class="product-main {{ ($loop->iteration % 3 == 2) ? '' : 'bg-blk' }}">
                             <div class="product-picture">
                                 <a href="{{ route('product_detail', $item->id) }}">
                                     @if ($primaryImg)
